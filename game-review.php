c04c48ec018c94ade279c12403b72dfbf14269e5
Marc Toensing <marc.toensing@gmail.com>
2021-12-01 23:13:09 +0100

    contributor fix

--- game-review.php
+++ game-review.php
@@ -5,7 +5,7 @@
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           1.0
- * Author:            The WordPress Contributors
+ * Author:            Marc Tönsing
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       game-review
