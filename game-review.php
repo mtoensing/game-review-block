da68060288dbdf8224bf206ed132a3e261c4a898
Marc Toensing <marc.toensing@gmail.com>
2021-12-01 23:09:59 +0100

    1.0

--- game-review.php
+++ game-review.php
@@ -4,7 +4,7 @@
  * Description:       Add a review rating block with a score from 1 to 10 to your post. Adds schema.org meta data for Rich Results in search engines.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
