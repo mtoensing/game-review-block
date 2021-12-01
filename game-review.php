daf08b0e5dee2eac78e28037c204c5e0b51a074a
Marc Toensing <marc.toensing@gmail.com>
2021-12-01 23:14:16 +0100

    Added url

--- game-review.php
+++ game-review.php
@@ -6,6 +6,7 @@
  * Requires PHP:      7.0
  * Version:           1.1
  * Author:            Marc Tönsing
+ * Author URI: 		  https://marc.tv
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       game-review
