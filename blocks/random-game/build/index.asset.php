94f010fc0e87ddfb6a7cf60529601f34497a6bc2
Marc Toensing <marc.toensing@gmail.com>
2022-01-13 21:22:03 +0100

    build

--- blocks/random-game/build/index.asset.php
+++ blocks/random-game/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'dcd5bd37fce9d8bdb0da0a9384d255e8');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '5dd8a09a4d5a92e2146af71a373d5c7f');
