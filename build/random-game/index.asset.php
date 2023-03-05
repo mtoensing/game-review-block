bf0646ac1e4f251915aa8a81d98c07ab08116c1c
Marc Toensing <marc.toensing@gmail.com>
2023-03-05 18:08:17 +0100

    dependencies updates

--- build/random-game/index.asset.php
+++ build/random-game/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ff1787a1951a19a8541e');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '866b9f751775693e3702');
