5a32df6000f7df1d2bcd9af137a04f26a0c88b7f
Marc Toensing <marc.toensing@gmail.com>
2023-05-12 18:38:10 +0200

    v 3-6

--- src/review-box/shortscore.js
+++ src/review-box/shortscore.js
@@ -1,44 +1,47 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get the elements
-    const ratingElem = document.querySelector(".shortscore");
-    const hreviewElem = document.querySelector(".shortscore-hreview");
+/* global IntersectionObserver */
+document.addEventListener( 'DOMContentLoaded', function () {
+	// Get the elements
+	const ratingElem = document.querySelector( '.shortscore' );
+	const hreviewElem = document.querySelector( '.shortscore-hreview' );
 
-    // Set the initial class value to 'shortscore-0'
-    ratingElem.className = 'shortscore shortscore-0';
+	// Set the initial class value to 'shortscore-0'
+	ratingElem.className = 'shortscore shortscore-0';
 
-    // Function to run the animation
-    const animateScore = () => {
-        let currentScore = 0;
-        const targetScore = parseInt(ratingElem.querySelector(".value").innerText);
+	// Function to run the animation
+	const animateScore = () => {
+		let currentScore = 0;
+		const targetScore = parseInt(
+			ratingElem.querySelector( '.value' ).innerText
+		);
 
-        const intervalID = setInterval(() => {
-            if (currentScore <= targetScore) {
-                ratingElem.className = `shortscore shortscore-${currentScore}`;
-                currentScore++;
-            } else {
-                clearInterval(intervalID);
-            }
-        }, 200);
-    };
+		const intervalID = setInterval( () => {
+			if ( currentScore <= targetScore ) {
+				ratingElem.className = `shortscore shortscore-${ currentScore }`;
+				currentScore++;
+			} else {
+				clearInterval( intervalID );
+			}
+		}, 200 );
+	};
 
-    // Create an observer
-    const observer = new IntersectionObserver(
-        (entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    animateScore();
-                    observer.unobserve(ratingElem);
-                }
-            });
-        },
-        { threshold: 1 }
-    );
+	// Create an observer
+	const observer = new IntersectionObserver(
+		( entries ) => {
+			entries.forEach( ( entry ) => {
+				if ( entry.isIntersecting ) {
+					animateScore();
+					observer.unobserve( ratingElem );
+				}
+			} );
+		},
+		{ threshold: 1 }
+	);
 
-    // Observe the element
-    observer.observe(ratingElem);
+	// Observe the element
+	observer.observe( ratingElem );
 
-    // Add click event listener
-    hreviewElem.addEventListener("click", () => {
-        animateScore();
-    });
-});
+	// Add click event listener
+	hreviewElem.addEventListener( 'click', () => {
+		animateScore();
+	} );
+} );
